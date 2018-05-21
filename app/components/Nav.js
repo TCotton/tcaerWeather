98f821e022b0700caff8d5d556c05e347a2eef7c
Andy Walpole <me@andywalpole.me>
2018-05-21 12:38:26 +0100

    added IndexLink nav

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactClass from 'create-react-class';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 const Nav = ReactClass({
 	render: function () {
 		return (
 			<div>
 				<h2>Nav component</h2>
-				<Link to='/'>Get Weather</Link>
-				<Link to='/about'>About</Link>
-				<Link to='/examples'>Examples</Link>
+				<IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+				<IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+				<IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
 			</div>
 		);
 	}
