5c23fd3bb2892644db83ec35b9500c8c4179e158
Andy Walpole <me@andywalpole.me>
2018-05-22 18:52:27 +0100

    more styling the page

--- app/components/Examples.js
+++ app/components/Examples.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Examples = () => {
 	return (
-		<h3>Examples</h3>
+		<div className='text-center'>
+			<h3>Examples</h3>
+			<p>Here are a few examples to try out:</p>
+			<ul>
+				<li>
+					<Link to='/?location=London'>London</Link>
+					<Link to='/?location=Brazil'>Brazil</Link>
+				</li>
+			</ul>
+		</div>
 	);
 };
 
